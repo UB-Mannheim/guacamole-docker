7b4d46031f893813a28e8bce616b3ab866cc9fac
Jörg Mechnich <joerg.mechnich@bib.uni-mannheim.de>
2020-03-23 12:00:31 +0100

    updated helper scripts

--- initdb.sh
+++ initdb.sh
@@ -2,8 +2,6 @@
 
 . ./.env
 
-DBCONT_NAME=guacamole_db_1
-
 if [ ! -e initdb.sql ]; then
     docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > initdb.sql
 fi
@@ -14,9 +12,17 @@ CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT SELECT,INSERT,UPDATE,DELETE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 EOF
 
-if [ -e backup.sql ]; then
-    SQLFILE=backup.sql
+if echo "SELECT 1 FROM guacamole_user LIMIT 1" | docker exec -i $DBCONT_NAME mysql $MYSQL_DATABASE > /dev/null 2>&1; then
+    echo "Database already initialized"
+    echo "Recreate empty database with"
+    echo " echo 'DROP DATABASE $MYSQL_DATABASE; CREATE DATABASE $MYSQL_DATABASE' | docker exec -i guacamole-docker_db_1 mysql"
 else
-    SQLFILE=initdb.sql
+    if [ -e backup.sql ]; then
+        SQLFILE=backup.sql
+        echo "Initializing database from $SQLFILE"
+    else
+        SQLFILE=initdb.sql
+        echo "Initializing database, login with guacadmin:guacadmin and change the password"
+    fi
+    docker exec -i $DBCONT_NAME mysql $MYSQL_DATABASE < $SQLFILE
 fi
-docker exec -i $DBCONT_NAME mysql $MYSQL_DATABASE < $SQLFILE
